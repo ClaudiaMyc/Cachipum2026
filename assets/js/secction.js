735de718f8d9ccc86078a8a65c432bb4de8084d1
claudia yanqui <claudiamyc94@gmail.com>
2024-02-26 10:26:29 -0300

    primerr ccommit}

---
+++ assets/js/secction.js
@@ -0,0 +1,9 @@
+// Inicializar variables
+let contador = 0;
+let maximo = 0;
+let ganados = 0;
+let perdidos = 0;
+let empatados = 0;
+
+alert("JUEGO" + "\n" + "CACHIPUN");
+maximo = +prompt("Cuantas veces quieres jugar? :", "6")
